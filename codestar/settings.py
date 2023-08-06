cff98238d3447f2127df729c8a7ff460a06b13d9
Rob Killick <robkillick@me.com>
2023-08-06 11:22:05 +0000

    update allowed host for heroku url

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rk-codestar-blog.herokuapp.com',
+ALLOWED_HOSTS = ['rk-codestar-blog-439546977f66.herokuapp.com',
                  '8000-rkillickdev-djangoblog-x47c846g0ck.ws-eu102.gitpod.io',
                  'localhost'
                  ]
